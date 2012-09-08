92ec858812e5b0021091b7f8a2aa4de02bf0d8cb
Eldar Gabdullin <eldargab@gmail.com>
2012-09-08 18:27:13 +0400

    move to 2 space tabs

--- examples/hello-world.js
+++ examples/hello-world.js
@@ -4,7 +4,7 @@ var app = App()
 app.get('/', 'send-hello')
 
 app.def('send-hello', function (get) {
-    get('response').send('Hello world!')
+  get('response').send('Hello world!')
 })
 
 app.listen(3000)
--- test/app.js
+++ test/app.js
@@ -3,62 +3,62 @@ var App = require('..')
 
 
 describe('App', function () {
-    var app
+  var app
 
-    beforeEach(function () {
-        app = App()
+  beforeEach(function () {
+    app = App()
+  })
+
+  it('Should dispatch routes to boxes', function (done) {
+    app
+      .get('/', 'hello')
+      .def('hello', function (get) {
+        get('response').send('hello')
+      })
+    request(app).get('/')
+      .expect(200)
+      .expect(/hello/, done)
+  })
+
+  it('Should respond with error on exception', function (done) {
+    app
+      .get('/error', 'error')
+      .def('error', function () {
+        throw new Error('my runtime error')
+      })
+    request(app).get('/error')
+      .expect(500)
+      .expect(/my runtime error/, done)
+  })
+
+  it('Should respond with error on undefined box', function (done) {
+    app.get('/', 'undefined')
+    request(app).get('/')
+      .expect(500)
+      .expect(/box/, done)
+  })
+
+  it('Should respond with 404 if there is no matched route', function (done) {
+    request(app).get('/').expect(404, done)
+  })
+
+  describe('Request', function () {
+    it('Should have .query default to {}', function (done) {
+      app.get('/', 'root').def('root', function (get) {
+        get('request').should.have.property('query').eql({})
+        get('response').end()
+      })
+      request(app).get('/').expect(200, done)
     })
 
-    it('Should dispatch routes to boxes', function (done) {
-        app
-        .get('/', 'hello')
-        .def('hello', function (get) {
-            get('response').send('hello')
+    it('Should contain the parsed query string', function (done) {
+      app.get('/', 'root').def('root', function (get) {
+        get('request').should.have.property('query').eql({
+          id: '1234'
         })
-        request(app).get('/')
-            .expect(200)
-            .expect(/hello/, done)
+        get('response').end()
+      })
+      request(app).get('/?id=1234').expect(200, done)
     })
-
-    it('Should respond with error on exception', function (done) {
-        app
-        .get('/error', 'error')
-        .def('error', function () {
-            throw new Error('runtime error')
-        })
-        request(app).get('/error')
-            .expect(500)
-            .expect(/runtime error/, done)
-    })
-
-    it('Should respond with error on undefined box', function (done) {
-        app.get('/', 'undefined')
-        request(app).get('/')
-            .expect(500)
-            .expect(/box/, done)
-    })
-
-    it('Should respond with 404 if there is no matched route', function (done) {
-        request(app).get('/').expect(404, done)
-    })
-
-    describe('Request', function () {
-        it('Should have .query default to {}', function (done) {
-            app.get('/', 'root').def('root', function (get) {
-                get('request').should.have.property('query').eql({})
-                get('response').end()
-            })
-            request(app).get('/').expect(200, done)
-        })
-
-        it('Should contain the parsed query string', function (done) {
-            app.get('/', 'root').def('root', function (get) {
-                get('request').should.have.property('query').eql({
-                    id: '1234'
-                })
-                get('response').end()
-            })
-            request(app).get('/?id=1234').expect(200, done)
-        })
-    })
+  })
 })
